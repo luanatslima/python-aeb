4ef39811aad2e00e835a71942898473b89104d64
luanatslima <luanatayna2007@gmail.com>
2026-04-25 13:55:14 -0300

    exercicios

---
+++ exercicios.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+altura = input("Digite sua altura: ")
+
+print(f"O seu nome é {nome};")
+print(f"Você tem {idade} anos;") 
+print(f"A sua altura é de {altura} metros")
